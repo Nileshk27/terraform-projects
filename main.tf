85a2ff51197c3edc28e660b1804c3395432c0788
Nileshk27 <124038890+Nileshk27@users.noreply.github.com>
2024-12-08 03:46:52 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
-provider aws {
+provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_vpc" "myVPC" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tr-vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet-1" {
+ vpc_id = aws_vpc.myVPC.id
+ cidr_block = "10.0.1.0/24"
+ availability_zone = "us-east-1a"
+ 
+ tags = {
+   Name = "tr-subnet"
+ }
+
+}
+
+resource "aws_instance" "sample" {
+  ami = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.my_subnet-1.id
+  key_name = "AWS_Prac"
 }
